008889816009437f2b58bc51d9a514445411fdab
erjan-A <ekazhman@gmail.com>
2014-09-12 00:21:07 +0600

    Added validation into User, Restaurant and Reviews forms

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,10 @@ class Restaurant < ActiveRecord::Base
 	mount_uploader :image, ImageUploader
 
 	has_many :reviews, dependent: :destroy
+
+	validates :name, :address, :phone, :website, :image, presence: true
+	validates :phone, format: { with: /\A\+\d \d{3} \d{3} \d{2} \d{2}\z/,
+															message: "must be in the format +7 123 456 78 90" }
+	validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
+															message: "must start with http:// or http://" }
 end
